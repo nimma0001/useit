47803b221f0aa95007af553889630bc46a152fe0
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-02-28 23:42:07 +0530

    Create it.sh

---
+++ it.sh
@@ -0,0 +1,25 @@
+echo "$1 uploading pixel"; \
+
+curl -T "$1" -u :3af751d9-ac45-46cd-b40c-ce0e21f3b238 \
+
+https://pixeldrain.com/api/file/; \
+
+echo "$1 uploaded pixel"; \
+
+echo "$1 UPLOADING DRIVE"; \
+
+rclone copy /app/bot/"$1" new:/February/"$2"; \
+
+echo "$1 uploaded drive"; \
+
+echo "$1 UPLOADING onedrive"; \
+
+rclone copy /app/bot/"$1" one:Public/February/"$2"; \
+
+echo "$1 uploaded onedrive"; \
+
+#telegram-upload --no-thumbnail --force-file --to "@myusersgbot" "$1"; \
+
+#echo "$1 uploaded to telegram"; \
+
+echo "script complete"
