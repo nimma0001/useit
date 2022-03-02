2594b8b32861e7bd547dd7536488da476aefb230
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-03-02 11:08:17 +0530

    Update did.sh

--- did.sh
+++ did.sh
@@ -5,12 +5,12 @@ curl -T "$1" -u :3af751d9-ac45-46cd-b40c-ce0e21f3b238 \
 https://pixeldrain.com/api/file/; \
 echo "$1 uploaded pixel"; \
 echo "$1 UPLOADING DRIVE"; \
-rclone copy /app/bot/"$1" new:/March/"$3"; \
+rclone copy /app/"$1" new:/March/"$3"; \
 echo "$1 uploaded drive"; \
 echo "$1 UPLOADING onedrive"; \
-rclone copy /app/bot/"$1" one:Public/March/"$3"; \
+rclone copy /app/"$1" one:Public/March/"$3"; \
 echo "$1 uploaded onedrive"; \
 #echo "$1 uploading to telegram"; \
-#telegram-upload --no-thumbnail --force-file --to "@myusersgbot" "/app/bot/$1"; \
+#telegram-upload --no-thumbnail --force-file --to "@myusersgbot" "/app/$1"; \
 echo "$1 uploaded to telegram"; \
 echo "script complete"
