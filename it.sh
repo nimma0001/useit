39cbf991281e778010280d7313375b068588c3b5
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-03-05 16:57:41 +0530

    U
    U

--- it.sh
+++ it.sh
@@ -1,6 +1,6 @@
 echo "$1 uploading pixel"; \
 curl -T "$1" -u :3af751d9-ac45-46cd-b40c-ce0e21f3b238 https://pixeldrain.com/api/file/; \
-echo "$1 uploaded pixel"; \
+echo "$1 uploaded pixel drain"; \
 echo "$1 UPLOADING DRIVE"; \
 rclone copy /app/"$1" new:/March/"$2"; \
 echo "$1 uploaded drive"; \
