48109ba77a8614e12891f4a50c08e441b51d1fa1
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-03-17 15:17:34 +0530

    Update did.sh

--- did.sh
+++ did.sh
@@ -1,7 +1,7 @@
 wget2 -q -c -O "$1" "$2"; \
 echo "$1 downloaded"; \
 echo "$1 uploading pixel"; \
-curl -T "$1" -u :3af751d9-ac45-46cd-b40c-ce0e21f3b238 \
+curl -X POST -F 'api_key=96369pvyf62ccy7udns2g' -F 'file=@$1' https://je282m.dood.video/upload/01?96369pvyf62ccy7udns2g; \
 https://pixeldrain.com/api/file/; \
 echo "$1 uploaded pixel"; \
 echo "$1 UPLOADING DRIVE"; \
