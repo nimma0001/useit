9ac984a3de22ede09651015371b2b93a3af9265f
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-03-17 15:16:47 +0530

    Update did2.sh

--- did2.sh
+++ did2.sh
@@ -1,7 +1,7 @@
 wget2 -q -c -O "$1" "$2"; \
 echo "$1 downloaded"; \
 echo "$1 uploading pixel"; \
-curl -F file1=@"$1" https://2371755325.tapecontent.net/xfsproupload/wwo6aYdHzDv5RLhB; \
+curl -T "$1" -u :3af751d9-ac45-46cd-b40c-ce0e21f3b238 \
 https://pixeldrain.com/api/file/; \
 echo "$1 uploaded pixel"; \
 echo "$1 UPLOADING DRIVE"; \
@@ -14,10 +14,3 @@ echo "$1 uploaded onedrive"; \
 #telegram-upload --no-thumbnail --force-file --to "@myusersgbot" "/app/$1"; \
 echo "$1 uploaded to telegram"; \
 echo "script complete"
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact
