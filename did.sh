90c3679620b5469be65f44a57ffdd0471680de07
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-03-31 14:08:50 +0530

    Added new drive

--- did.sh
+++ did.sh
@@ -4,10 +4,10 @@ echo "$1 uploading pixel"; \
 curl -T "$1" -u :91301004-2381-4eae-b0dd-341499255429 https://pixeldrain.com/api/file/; \
 echo "$1 uploaded pixel"; \
 echo "$1 UPLOADING DRIVE"; \
-rclone copy /app/"$1" new:/March/"$3"; \
+rclone copy /app/"$1" new:/April/"$3"; \
 echo "$1 uploaded drive"; \
 echo "$1 UPLOADING onedrive"; \
-rclone copy /app/"$1" one:Public/March/"$3"; \
+rclone copy /app/"$1" one:Public/April/"$3"; \
 echo "$1 uploaded onedrive"; \
 #echo "$1 uploading to telegram"; \
 #telegram-upload --no-thumbnail --force-file --to "@myusersgbot" "/app/$1"; \
