784a478f64b1252bfe3bf6742f7a66f292cb1394
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-04-26 16:42:41 +0530

    Update did.sh

--- did.sh
+++ did.sh
@@ -4,7 +4,7 @@ echo "$1 uploading pixel"; \
 curl -T "$1" -u :91301004-2381-4eae-b0dd-341499255429 https://pixeldrain.com/api/file/; \
 echo "$1 uploaded pixel"; \
 echo "$1 UPLOADING DRIVE"; \
-rclone copy /app/"$1" new:/April/"$3"; \
+#rclone copy /app/"$1" new:/April/"$3"; \
 echo "$1 uploaded drive"; \
 echo "$1 UPLOADING onedrive"; \
 rclone copy /app/"$1" one:Public/April/"$3"; \
