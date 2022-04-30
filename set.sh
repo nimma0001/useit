ea03b4eaf7974db41ff983efffa6526fd6cb64f2
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-04-30 20:52:40 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,5 +1,7 @@
 #mkdir -p /app/.config; \
 #mv .config/* /app/.config/; \
 rclone mkdir one:Public/April/$1; \
+echo "Make folder done"; \
+rclone copyto one: one2:; \
 #rclone mkdir new:/April/$1; \
 echo done
