bde2f0c659004bd20ec434d40d39bc547e42edd7
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-05-02 18:37:21 +0530

    Update it.sh

--- it.sh
+++ it.sh
@@ -1,5 +1,5 @@
 echo "$1 uploading pixel"; \
-curl -T "$1" -u :91301004-2381-4eae-b0dd-341499255429 https://pixeldrain.com/api/file/; \
+curl -T "$1" -u :8d92bb15-fba1-45d3-9a48-8a754ff11a68 https://pixeldrain.com/api/file/; \
 #curl -X POST -F 'api_key=96369pvyf62ccy7udns2g' -F 'file=@$1' https://je282m.dood.video/upload/01?96369pvyf62ccy7udns2g; \
 echo "$1 uploaded pixel drain"; \
 #echo "$1 UPLOADING DRIVE"; \
