2cb56f6555863be663321e7c7e2fdc665d9e8f93
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-05-14 22:44:58 +0530

    Update ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -2,5 +2,5 @@ yt-dlp -S +res:"$1",+ext:mp4  --prefer-ffmpeg -o "$2.mp4" "$3"; \
  echo "download completed $1"; \
 echo "$2 uploading pixel"; \
 mv "$2.mkv" "$2.mp4"; \
-curl -T "$2.mp4" -u :011ac321-1c21-4aa0-8608-ea69c55fa7ea https://pixeldrain.com/api/file/; \
+curl -T "$2.mp4" -u :"50281f27-e8f4-4e1c-bb05-b6bfd5a4bf17" https://pixeldrain.com/api/file/; \
 echo "$2 uploaded pixel"; \
