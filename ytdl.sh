bf50012e532d6fabcc140d635731acdea7d6e1d1
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-05-14 22:53:03 +0530

    Update ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -1,4 +1,4 @@
-yt-dlp -S +res:"$1",+ext:mp4  --prefer-ffmpeg -o "$2.mp4" "$3"; \
+yt-dlp -S +res:"$1",+ext:webm  --prefer-ffmpeg -o "$2.mp4" "$3"; \
  echo "download completed $1"; \
 echo "$2 uploading pixel"; \
 mv "$2.mkv" "$2.mp4"; \
