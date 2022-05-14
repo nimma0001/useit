56ef2d3b407aea4c15405c0c489ebbff4c995dee
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-05-14 23:01:54 +0530

    Update ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -1,6 +1,9 @@
 yt-dlp -S +res:"$1",+ext:webm  --prefer-ffmpeg -o "$2.mp4" "$3"; \
- echo "download completed $1"; \
+echo "Nirmal big penis"; \
+mv "$2.mp4.web" "$2.mp4"; \
+echo "download completed $1"; \
 echo "$2 uploading pixel"; \
 mv "$2.mkv" "$2.mp4"; \
 curl -T "$2.mp4" -u :"50281f27-e8f4-4e1c-bb05-b6bfd5a4bf17" https://pixeldrain.com/api/file/; \
 echo "$2 uploaded pixel"; \
+echo "Nirmal big penis has penetrated the world"; \
