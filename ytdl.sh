925c6dcf4df4ca14b731b077578aa7f8ee186b30
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-05-17 08:06:19 +0530

    Update ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -1,6 +1,6 @@
 yt-dlp -S +res:"$1",+ext:webm  --prefer-ffmpeg -o "$2.mp4" "$3"; \
 echo "Nirmal big penis"; \
-mv "$2.mp4.web" "$2.mp4"; \
+mv "$2.mp4.webm" "$2.mp4"; \
 echo "download completed $1"; \
 echo "$2 uploading pixel"; \
 mv "$2.mkv" "$2.mp4"; \
